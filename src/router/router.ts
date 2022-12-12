c9ec8525a631d54413f0fde79d4e4d2a31ed6965
IvayloEntropy <entropyactive@gmail.com>
2022-12-12 12:05:04 +0300

    add blog post

--- src/router/router.ts
+++ src/router/router.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router"
 import Home from "../views/Home.vue"
 import Contacts from "../views/Contacts.vue"
 import Articles from "../views/Articles.vue"
+import Article from "../views/Article.vue"
+
 import Project from "../views/Project.vue"
 import Projects from "../views/Projects.vue"
 
@@ -10,6 +12,8 @@ import About from "../views/About.vue"
 const routes = [
   { path: "/", component: Home },
   { path: "/articles", component: Articles },
+  { path: "/blog-post", component: Article },
+
   { path: "/contacts", component: Contacts },
   { path: "/projects", component: Projects },
   { path: "/project", component: Project },
