960359cf6c34bad14d64366f5c35401cfcb3ec35
Ivaylo Ovcharov <50172438+IvayloEntropy@users.noreply.github.com>
2024-04-19 11:39:05 +0100

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,11 @@
 import axios from "axios"
 import Airtable from "airtable"
-var base = new Airtable({ apiKey: "keyRwzPKxBwoAGIlj" }).base(
+var base = new Airtable({ apiKey: "patuD6f740CQdsH8c.9f5726d69111f3b0d8a81d88b832dbd0659d77eb0eabf9555b33fe6b0118798c" }).base(
   "app4oxLvO7wwYKKFc",
 )
 
 export function load(entity) {
-  const apiToken = "keyRwzPKxBwoAGIlj"
+  const apiToken = "patuD6f740CQdsH8c.9f5726d69111f3b0d8a81d88b832dbd0659d77eb0eabf9555b33fe6b0118798c"
   return axios.get(
     `https://api.airtable.com/v0/app4oxLvO7wwYKKFc/${entity}?maxRecords=50&view=Grid%20view`,
     {
